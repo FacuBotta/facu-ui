d52b912f6c98ec7d90ffc4e0a4bbaf1e7620c15d
facubotta <nutuestampados@gmail.com>
2024-11-15 08:07:43 +0100

    changes to merge

--- src/components/icon/iconsProps.ts
+++ src/components/icon/iconsProps.ts
@@ -1,4 +1,3 @@
-
 export type iconProps = {
   className?: string;
   /** Set if the icon is filled or not, default is false. The behavior can be different for different icons */
@@ -12,32 +11,70 @@ export type iconProps = {
   cursor?: string;
   strokeWidth?: number;
   onClick?: React.MouseEventHandler<SVGSVGElement>;
-  type?: 'instagram' | 'youtube' | 'search' | 'menu' | 'burgerMenu' | 'plus' | 'minus' | 'heart' | 'star' | 'send' | 'userOff' | 'message' | 'add' | 'addUser' | 'addPhoto' | 'edit' | 'logIn' | 'logOut' | 'alarm' | 'delete' | 'user' | 'linkedIn' | 'home' | 'calendar' | 'calendarPlus' | 'eye' | 'eyeOff' | 'mapSearch' | 'mapPin' | 'infoCircle' | 'phone' | 'bell' | 'twitter' | 'faceBook' | 'tikTok' | 'discord';
-}
+  type?:
+    | "sun"
+    | "moon"
+    | "instagram"
+    | "youtube"
+    | "search"
+    | "menu"
+    | "burgerMenu"
+    | "plus"
+    | "minus"
+    | "heart"
+    | "star"
+    | "send"
+    | "userOff"
+    | "message"
+    | "add"
+    | "addUser"
+    | "addPhoto"
+    | "edit"
+    | "logIn"
+    | "logOut"
+    | "alarm"
+    | "delete"
+    | "user"
+    | "linkedIn"
+    | "home"
+    | "calendar"
+    | "calendarPlus"
+    | "eye"
+    | "eyeOff"
+    | "mapSearch"
+    | "mapPin"
+    | "infoCircle"
+    | "phone"
+    | "bell"
+    | "twitter"
+    | "faceBook"
+    | "tikTok"
+    | "discord";
+};
 
 export const svgProps = (props: iconProps) => {
-  const filled = props.filled? 'currentColor' : 'none'
+  const filled = props.filled ? "currentColor" : "none";
   return {
     viewBox: "0 0 24 24",
-    width: props.width || '24',
-    height: props.width || '24',
+    width: props.width || "24",
+    height: props.width || "24",
     fill: filled,
-    stroke: props.color || 'currentColor',
+    stroke: props.color || "currentColor",
     strokeWidth: props.strokeWidth || 1,
     // strokeLinecap: "round",
     // strokeLinejoin: "round",
-    style: { cursor: props.cursor || 'pointer' },
+    style: { cursor: props.cursor || "pointer" },
     onMouseOver: (e: React.MouseEvent<SVGSVGElement>) => {
       if (props.filled) {
-        e.currentTarget.style.fill = props.hoverColor || 'currentColor'
+        e.currentTarget.style.fill = props.hoverColor || "currentColor";
       }
-      e.currentTarget.style.stroke = props.hoverColor || 'currentColor'
+      e.currentTarget.style.stroke = props.hoverColor || "currentColor";
     },
     onMouseLeave: (e: React.MouseEvent<SVGSVGElement>) => {
       if (props.filled) {
-        e.currentTarget.style.fill = 'currentColor'
+        e.currentTarget.style.fill = "currentColor";
       }
-      e.currentTarget.style.stroke = 'currentColor'
+      e.currentTarget.style.stroke = "currentColor";
     },
-  }
+  };
 };
